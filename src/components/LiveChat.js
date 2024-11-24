cc11e0e1965c8a24e31c49b61d7bdca947a7ef5b
jpofficials7 <jpofficials7@gmail.com>
2024-11-24 17:26:55 +0530

    updates

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/chatSlice';
 import { generateRandomName, generateRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState([]);
   const dispatch = useDispatch();
   const ChatMessages = useSelector((store) => store.chat.messages);
 
@@ -19,11 +20,27 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll">
-      {ChatMessages &&
-        ChatMessages.length > 0 &&
-        ChatMessages.map((message, index) => <ChatMessage key={index} name={message.name} message={message.message} />)}
-    </div>
+    <>
+      <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {ChatMessages &&
+            ChatMessages.length > 0 &&
+            ChatMessages.map((message, index) => (
+              <ChatMessage key={index} name={message.name} message={message.message} />
+            ))}
+        </div>
+      </div>
+      <div className="w-full p-2 ml-2 border border-black">
+        <input
+          className="px-2 w-96"
+          type="text"
+          placeholder="Type your message here"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </div>
+    </>
   );
 };
 
