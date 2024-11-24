48a0608d485fc66aaeac4d1b93648efb4dc2edbd
jpofficials7 <jpofficials7@gmail.com>
2024-11-24 17:12:45 +0530

    updates

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,27 +1,28 @@
 import { useEffect } from 'react';
 import ChatMessage from './ChatMessage';
-
-const data = [];
+import { useDispatch, useSelector } from 'react-redux';
+import { addMessage } from '../utils/chatSlice';
+import { generateRandomName, generateRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const ChatMessages = useSelector((store) => store.chat.messages);
+
   useEffect(() => {
     const i = setInterval(() => {
       // API Polling
       console.log('Polling for new messages');
+      dispatch(addMessage({ name: generateRandomName(), message: generateRandomMessage(30) }));
     }, 2000);
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
-      <ChatMessage name="akshay Saini" message="This is Namaste React Live!🙏" />
+    <div className="w-full h-[550px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll">
+      {ChatMessages &&
+        ChatMessages.length > 0 &&
+        ChatMessages.map((message, index) => <ChatMessage key={index} name={message.name} message={message.message} />)}
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const GOOGLE_API_KEY = 'AIzaSyBrB1_KXQ39Dba8oU4KzFuKUYgn2auF3pY';
 
+export const LIVE_CHAT_COUNT = 10;
+
 export const YOUTUBE_VIDEOS_API =
   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' +
   GOOGLE_API_KEY;
