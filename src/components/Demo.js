1c2caa04bdb573e37cb7b637c70da8b895aeffca
jpofficials7 <jpofficials7@gmail.com>
2024-11-27 09:56:53 +0530

    updates

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,17 +1,41 @@
 import { useState } from 'react';
 
+import { findNthPrime } from '../utils/helper';
+
 const Demo = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(0);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  console.log('Rendering...');
+  // heavy operation
+
+  const prime = () => {
+    console.log('Calculate Prime Number of', text);
+    return findNthPrime(text);
+  };
+
   return (
-    <div className="m-4 p-2 w-96 h-96 border border-black">
+    <div className={'m-4 p-2 w-96 h-96 border border-black ' + (isDarkTheme && ' bg-gray-900')}>
+      <div>
+        <button
+          className="m-10 p-2 bg-green-200"
+          onClick={() => {
+            setIsDarkTheme(!isDarkTheme);
+          }}>
+          Toggle
+        </button>
+      </div>
       <div>
         <input
           className="border border-black w-72 px-2"
-          type="text"
+          type="number"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="Enter Text"
         />
+      </div>
+      <div>
+        <h1 className="mt-4 font-bold text-xl text-blue-600">nth Prime:{prime()}</h1>
       </div>
     </div>
   );
